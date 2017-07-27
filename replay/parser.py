daf037c7055e056835801a84e08f4a212b46164c
hanhanW <hanhan0912@gmail.com>
2017-07-28 07:44:50 +0800

    Fix pcap parser

--- replay/parser.py
+++ replay/parser.py
@@ -31,13 +31,13 @@ if __name__ == '__main__':
     streams = PcapParser('hello.pcap').get_streams()
     for i in streams:
         counter = [0, 0]
-        res = {}
+        res = []
         for a,b in i:
-            key = 'peer' + str(a) + '_' + str(counter[a])
-            counter[a] += 1
             val = base64.b64encode(str(b[Raw])) if Raw in b else ''
-            res[key] = val
+            res.append({ 'id': a, 'counter': counter[a], 'timestamp': b.time, 'data': val })
+            counter[a] += 1
         fname = 'pcap_' + str(i[1][1].getlayer('TCP').sport) + '_' + str(int(i[1][1].time)) + '.json'
-        f = file(fname, 'w')
+        fname = os.path.join('stream', fname)
         print 'Save Packet Stream:',fname
+        f = file(fname, 'w')
         json.dump(res, f)
