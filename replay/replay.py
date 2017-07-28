5e3c873d07e31be64204d108723d65124ccbe9c2
lyc12345 <b00507010@ntu.edu.tw>
2017-07-28 13:06:56 +0800

    paralell better version

--- replay/replay.py
+++ replay/replay.py
@@ -23,27 +23,27 @@ def check(name):
     for r in res:
         if len(r['data'])!=0 and r['id'] == 0: payloads.append(r['data'].decode('base64'))
         if len(r['data'])!=0 and r['id'] == 1: response.append(r['data'].decode('base64'))
-    if os.fork() > 0:
-        cmd = 'timeout 3 ./clemency-emu bin/'+binary+' -l '+str(n)+' > /tmp/'+proc.name
-        os.system(cmd)
-        out = open('/tmp/'+proc.name).read()
-        if 'exception' in out or 'Exception' in out:
-            print out
-            print '!'
-        else:
-            print 'ok'
-    else: 
-        sleep(0.1)
-        r = remote('127.0.0.1',n)
-        try:
-            for payload in  payloads:
-                r.send(payload)
-                r.recvrepeat(0.2)
-                #print repr(r.recvrepeat(0.2))
-        except:
-            pass
-        #for resp in response: print repr(resp)
-        exit(1)
+    
+    cmd = './clemency-emu bin/'+binary+' -L '+str(n)+' > /tmp/'+proc.name
+    process = subprocess.Popen(cmd, shell=True)
+    
+    sleep(0.1)
+    r = remote('127.0.0.1',n)
+    try:
+        for payload in  payloads:
+            r.send(payload)
+            r.recvrepeat(0.2)
+            #print repr(r.recvrepeat(0.2))
+    except:
+        pass
+    #for resp in response: print repr(resp)
+    
+    out = open('/tmp/'+proc.name).read()
+    if 'exception' in out or 'Exception' in out:
+        print out
+        print '!'
+    else:
+        print 'ok'
 
-p = Pool(2)
+p = Pool(10)
 p.map(check,os.listdir(dirname))
