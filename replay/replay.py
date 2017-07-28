d9fec82a780f1fff0efaf188ec732655a6496090
hanhanW <hanhan0912@gmail.com>
2017-07-28 16:56:08 +0800

    zzz

--- replay/replay.py
+++ replay/replay.py
@@ -12,6 +12,14 @@ binary = sys.argv[1]
 port = sys.argv[2]
 dirname = 'stream/'+str(port)
 
+if not os.path.exists(os.path.join('bin', binary)):
+    print 'Binary bin/%s not exists' % binary
+    exit(1)
+
+if not os.path.exists(dirname):
+    print 'Dir %s not exists' % dirname
+    exit(1)
+
 def check(name):
     proc = current_process()
     print proc.name
@@ -24,8 +32,10 @@ def check(name):
         if len(r['data'])!=0 and r['id'] == 0: payloads.append(r['data'].decode('base64'))
         if len(r['data'])!=0 and r['id'] == 1: response.append(r['data'].decode('base64'))
     
-    cmd = './clemency-emu bin/'+binary+' -L '+str(n)+' > /tmp/'+proc.name
-    process = subprocess.Popen(cmd, shell=True)
+    cmd = './clemency-emu-no-access-flag bin/'+binary+' -L '+str(n)+' -d 0 > /tmp/'+proc.name
+    p = process(cmd, shell=True)
+    p.sendline('g')
+    p.sendline('q')
     
     sleep(0.1)
     r = remote('127.0.0.1',n)
@@ -38,12 +48,22 @@ def check(name):
         pass
     #for resp in response: print repr(resp)
     
+    p.close()
     out = open('/tmp/'+proc.name).read()
     if 'exception' in out or 'Exception' in out:
-        print out
-        print '!'
+        flag = False
+        for i in range(1000):
+            s = '4010%0x' % i
+            if s in out:
+                print 'Find %s' % s
+                flag = True
+                break
+        if flag:
+            print 'leak flag'
+        else:
+            print 'crash'
     else:
         print 'ok'
 
-p = Pool(10)
+p = Pool(2)
 p.map(check,os.listdir(dirname))
