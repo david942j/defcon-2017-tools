1c502e25ee42991957f93a473886895ccc32d530
hanhanW <hanhan0912@gmail.com>
2017-07-28 08:11:03 +0800

    Save the streams to relative prob_id

--- replay/parser.py
+++ replay/parser.py
@@ -1,5 +1,7 @@
 import base64
 import json
+import os
+import hashlib
 from scapy.all import *
 from collections import defaultdict
 
@@ -29,6 +31,8 @@ class PcapParser(object):
 
 if __name__ == '__main__':
     streams = PcapParser('hello.pcap').get_streams()
+    if not os.path.exists('stream'):
+        os.mkdir('stream')
     for i in streams:
         counter = [0, 0]
         res = []
@@ -36,8 +40,13 @@ if __name__ == '__main__':
             val = base64.b64encode(str(b[Raw])) if Raw in b else ''
             res.append({ 'id': a, 'counter': counter[a], 'timestamp': b.time, 'data': val })
             counter[a] += 1
-        fname = 'pcap_' + str(i[1][1].getlayer('TCP').sport) + '_' + str(int(i[1][1].time)) + '.json'
-        fname = os.path.join('stream', fname)
+        prob_id = str(i[1][1].getlayer('TCP').sport)
+        path = os.path.join('stream', prob_id)
+        if not os.path.exists(path):
+            os.mkdir(path)
+        md5 = hashlib.md5(json.dumps(res)).hexdigest()
+        fname = md5 + '.json'
+        fname = os.path.join(path, fname)
         print 'Save Packet Stream:',fname
         f = file(fname, 'w')
         json.dump(res, f)
