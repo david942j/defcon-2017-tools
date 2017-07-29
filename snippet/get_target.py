d24a013f6ab324f5cff446398e290c0fcc3652f3
shik <shik.chen@gmail.com>
2017-07-29 13:52:30 -0700

    clarify get target

--- snippet/get_target.py
+++ snippet/get_target.py
@@ -1,8 +1,7 @@
 def get_target(default_port):
-    assert default_port != 99999
     import sys
     host = sys.argv[1] if len(sys.argv) >= 2 else '127.0.0.1'
     port = int(sys.argv[2]) if len(sys.argv) >= 3 else default_port
     return host, port
 
-# r = remote(*get_target(99999))
+# r = remote(*get_target(CHANGE_ME_TO_SERVICE_PORT))
